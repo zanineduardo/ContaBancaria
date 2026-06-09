28b8260c25e823a61da29fd7c4fda8c8a1bcda9c
Aluno <aluno@senai.br>
2026-06-09 19:15:25 -0300

    salvar

--- src/main/java/ContaBancaria.java
+++ src/main/java/ContaBancaria.java
@@ -11,5 +11,5 @@ public class ContaBancaria { //
 
     public double getSaldo(){
         return saldo;
-    }
+    } //a
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,7 @@
 public class Main {
+    static void main(String[] args) {
+        ContaBancaria minhaGrana = new ContaBancaria();
+        minhaGrana.depositar(500);
+        System.out.println("Tenho R$ " + minhaGrana.getSaldo() );
+    }
 }
