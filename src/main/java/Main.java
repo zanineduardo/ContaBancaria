be555001adaf7b994168cd70113a923508cdd7a1
Aluno <aluno@senai.br>
2026-06-09 22:11:02 -0300

    salvar

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,26 @@
+import java.util.Scanner;
+
 public class Main {
-    static void main(String[] args) {
-        ContaBancaria minhaGrana = new ContaBancaria();
-        minhaGrana.depositar(500);
-        System.out.println("Tenho R$ " + minhaGrana.getSaldo() );
+    public static void main(String[] args) {
+
+        int opcao;
+
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.println("Escolha uma das 3 opções.\n1. Depositar 2. Sacar. 0. Sair");
+        opcao = teclado.nextInt();
+
+        do {
+            if (opcao == 1) {
+                minhaGrana.depositar();
+
+                System.out.println("Você tem R$ " + minhaGrana.getSaldo() );
+            }
+            else if (opcao == 2){
+                minhaGrana.sacar(100);
+                System.out.println("Sobrou R$ " + minhaGrana.getSaldo() );
+            }
+        } while (opcao != 0);
+
     }
 }
