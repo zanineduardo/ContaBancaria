758b612ab9d09cfdd5ad2bbe10549f0c88f11228
Aluno <lkdasjkld@senai.br>
2026-06-11 19:59:50 -0300

    Aula 3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,26 +1,39 @@
 import java.util.Scanner;
 
+
 public class Main {
     public static void main(String[] args) {
 
-        int opcao;
 
+        int opcao;
         Scanner teclado = new Scanner(System.in);
 
-        System.out.println("Escolha uma das 3 opções.\n1. Depositar 2. Sacar. 0. Sair");
-        opcao = teclado.nextInt();
+
+        ContaBancaria minhaGrana = new ContaBancaria();
+
 
         do {
+            System.out.println("\n Escolha uma das 3 opções.\n1. Depositar \n2. Sacar \n0. Sair\n");
+            opcao = teclado.nextInt();
+
+
             if (opcao == 1) {
-                minhaGrana.depositar();
+                System.out.println("Digite o valor de seu depósito: ");
+                double valor = teclado.nextDouble();
+                minhaGrana.depositar(valor);
+                System.out.println("Você tem R$ " + minhaGrana.getSaldo());
 
-                System.out.println("Você tem R$ " + minhaGrana.getSaldo() );
-            }
-            else if (opcao == 2){
-                minhaGrana.sacar(100);
-                System.out.println("Sobrou R$ " + minhaGrana.getSaldo() );
-            }
-        } while (opcao != 0);
 
+            } else if (opcao == 2) {
+                System.out.println("Digite o valor de seu saque: ");
+                double valor = teclado.nextDouble();
+                minhaGrana.sacar(valor);
+                System.out.println("Agora você tem R$ " + minhaGrana.getSaldo());
+            }
+            else if (opcao == 0) {
+                System.out.println("Você escolheu sair. Obrigado por usar nosso sistema!");
+            }
+        } while (opcao != 0); //git
     }
 }
+
